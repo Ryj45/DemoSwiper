897daa54ce1bd14c17da88555cfd6f709e8c5bff
18039253 <18039253@myrp.edu.sg>
2020-07-23 09:38:49 +0800

    Initial commit

---
+++ app/src/main/java/c347/rp/edu/sg/demoswiper/frag2.java
@@ -0,0 +1,28 @@
+package c347.rp.edu.sg.demoswiper;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class frag2 extends Fragment {
+
+    public frag2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
